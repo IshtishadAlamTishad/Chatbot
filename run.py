f9dafbb7fba88aad4c307f76dd67423c5664c0d9
IshtishadAlamTishad <tishadalam86@gmail.com>
2025-08-21 17:57:03 +0600

    Added loop functionality in run.py

--- run.py
+++ run.py
@@ -1,18 +1,22 @@
 from module.bot import Chatbot
-from module.bot import Chatbot
 from module.VD import Db
-from module.se import LanguageToIndex,IndexToLanguage,MaxSequenceLength,StartToken,EndToken,PaddingToken
+from module.se import LanguageToIndex, IndexToLanguage, MaxSequenceLength, StartToken, EndToken, PaddingToken
 
 model = 'model/trainedModel.pth'
 
-xc = Chatbot(Db,model,LanguageToIndex,IndexToLanguage,MaxSequenceLength,StartToken,EndToken,PaddingToken)
+xc = Chatbot(Db, model, LanguageToIndex, IndexToLanguage, MaxSequenceLength, StartToken, EndToken, PaddingToken)
 
 print(xc.ask("What about debt?"))
 print(xc.ask("Tell me about infrastructure."))
+print("\nType 'exit' to stop the program.\n")
 
-k = input("Enter your question: ")
-print(xc.ask(k))    
+i = 1
+while True:
+    k = input("Enter your question: ")
+    
+    if k.lower() == "exit":
+        print("Exited!")
+        break
 
-
-
-
+    print(f"{i} → {xc.ask(k)}")
+    i += 1
